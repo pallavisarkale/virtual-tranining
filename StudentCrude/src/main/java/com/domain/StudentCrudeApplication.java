c90d5d9e5f2f3ee7feb85a837faf5235fc33e409
pallavisarkale <78130154+pallavisarkale@users.noreply.github.com>
2021-04-29 10:49:26 +0530

    initial commit

---
+++ StudentCrude/src/main/java/com/domain/StudentCrudeApplication.java
@@ -0,0 +1,13 @@
+package com.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrudeApplication.class, args);
+	}
+
+}
---
+++ StudentCrude/src/main/java/com/domain/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.domain.entity.Student;
+ 
+public interface StudentRepository extends JpaRepository<Student, Long> {
+	 
+}
